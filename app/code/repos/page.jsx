7ec4ea124b5519c7c961b2110d61e225a1bef157
rakibul-wdp <rakibul.wdp@gmail.com>
2022-08-21 04:05:00 +0600

    use next revalidate

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -3,7 +3,11 @@ import Link from 'next/link';
 import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
 
 async function fetchRepos() {
-  const response = await fetch("https://api.github.com/users/rakibul-wdp/repos");
+  const response = await fetch("https://api.github.com/users/rakibul-wdp/repos", {
+    next: {
+      revalidate: 60,
+    }
+  });
 
   await new Promise((resolve) => setTimeout(resolve, 1000)); // wait 1 second for see loader
 
--- app/components/RepoDirs.jsx
+++ app/components/RepoDirs.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 async function fetchRepoContents(name) {
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  const response = await fetch(`https://api.github.com/repos/rakibul-wdp/${name}/contents`);
+  const response = await fetch(`https://api.github.com/repos/rakibul-wdp/${name}/contents`, {
+    next: {
+      revalidate: 60,
+    }
+  });
   const contents = await response.json();
 
   return contents;
