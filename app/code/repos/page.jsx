0e46f8e9d6e7b2b079dff5eaf74763bc2411cb8c
rakibul-wdp <rakibul.wdp@gmail.com>
2022-09-06 04:05:00 +0600

    create loader and do not need to import or by use usestate

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -4,6 +4,9 @@ import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
 
 async function fetchRepos() {
   const response = await fetch("https://api.github.com/users/rakibul-wdp/repos");
+
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // wait 1 second for see loader
+
   const repos = await response.json();
   return repos;
 }
