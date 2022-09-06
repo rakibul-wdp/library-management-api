0e5f00694f4f0335b343668a7f926d0701c14aba
rakibul-wdp <rakibul.wdp@gmail.com>
2022-09-06 06:05:00 +0600

    create RepoDirs and use suspense from react

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,12 +1,18 @@
 import Repo from '@/app/components/Repo';
+import RepoDirs from '@/app/components/RepoDirs';
 import Link from 'next/link';
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const RepoPage = ({ params: { name } }) => {
   return (
     <div className='card'>
       <Link href="/code/repos" className='btn btn-back'>Back To Repositories</Link>
-      <Repo name={name} />
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading directories...</div>}>
+        <RepoDirs name={name} />
+      </Suspense>
     </div>
   )
 }
