271dd22aee8f8d3ebbfc74ca625f7d587774aa7a
rakibul-wdp <rakibul.wdp@gmail.com>
2022-09-14 04:05:00 +0600

    use google font via next

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,10 @@
-import './globals.css'
+import { Poppins } from "next/font/google";
+import './globals.css';
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"]
+})
 
 export const metadata = {
   title: 'rakibul-wdp repo',
@@ -9,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
