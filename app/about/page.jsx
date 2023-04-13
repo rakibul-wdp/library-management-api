6a36423506d294a7a9e675d3eb70679a66309c96
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-13 10:26:15 +0600

    routing and custoom layout

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About rakibul-wdp Repo</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum veritatis odio laboriosam voluptatem libero quasi rem repellat quos? Sapiente, saepe odit voluptatum id ea in delectus repellendus cumque autem. Quaerat.</p>
+    </div>
+  )
+}
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, soluta dolores nulla assumenda enim velit amet tempore iste omnis fugiat fugit quas cupiditate, ipsa est! Voluptatem doloremque eligendi odio eum.</p>
+    </div>
+  )
+}
+
+export default TeamPage;
