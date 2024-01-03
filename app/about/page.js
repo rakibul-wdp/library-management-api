c08ea4839cb3f586e7ec818b0d918af0dc0dc5d0
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-03 16:30:41 +0600

    error handle

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,6 @@
-export default function page() {
+export default function About() {
+  throw new Error("There was a error!");
+
   return (
     <main className="mt-5">
       <h1>About Us page</h1>
