f05847e13c6172f4f401392747971cc92f891290
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-03 16:52:15 +0600

    important notes of loading error and not-found component use case

---
+++ app/blogs/[id]/not-found.js
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <div>blog was not found!!</div>;
+}
--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,5 +1,11 @@
+import { notFound } from "next/navigation";
+
 export default function Blog({ params }) {
   const { id } = params;
+
+  if (id >= "3") {
+    notFound();
+  }
 
   return <div className="mt-5">The blog id is: {id}</div>;
 }
