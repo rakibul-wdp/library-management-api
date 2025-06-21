c1cca99cd5336a9ca19ac0941fe7ae2b5a6140c8
rakibul-wdp <rakibul.wdp@gmail.com>
2025-06-21 12:30:02 +0600

    create book model

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,70 @@
+import { model, Schema } from "mongoose";
+import { BookGenre, IBook, BookModel } from "../interfaces/book.interface";
+
+const bookSchema = new Schema<IBook, BookModel>(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    author: {
+      type: String,
+      required: [true, "Author is required"],
+    },
+    genre: {
+      type: String,
+      required: true,
+      enum: {
+        values: [
+          "FICTION",
+          "NON_FICTION",
+          "SCIENCE",
+          "HISTORY",
+          "BIOGRAPHY",
+          "FANTASY",
+        ],
+        message: "{VALUE} is not a valid genre",
+      },
+    },
+    isbn: {
+      type: String,
+      required: [true, "ISBN is required"],
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+    copies: {
+      type: Number,
+      required: [true, "Copies count is required"],
+      min: [0, "Copies cannot be negative"],
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+bookSchema.static("updateAvailability", async function (bookId: string) {
+  const book = await this.findById(bookId);
+  if (book && book.copies <= 0) {
+    book.available = false;
+    await book.save();
+  }
+});
+
+bookSchema.pre("save", function (next) {
+  if (this.copies <= 0) {
+    this.available = false;
+  } else {
+    this.available = true;
+  }
+  next();
+});
+
+export const Book = model<IBook, BookModel>("Book", bookSchema);
