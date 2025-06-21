1ce36d8beb2220e2ebe22d3aebacda8b52cc75bc
rakibul-wdp <rakibul.wdp@gmail.com>
2025-06-21 12:38:05 +0600

    add get borrow books summary controller

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -51,3 +51,48 @@ export const borrowBook = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const getBorrowedBooksSummary = async (req: Request, res: Response) => {
+  try {
+    const summary = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
+      },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookDetails",
+        },
+      },
+      {
+        $unwind: "$bookDetails",
+      },
+      {
+        $project: {
+          book: {
+            title: "$bookDetails.title",
+            isbn: "$bookDetails.isbn",
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
+      data: summary,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error retrieving borrowed books summary",
+      success: false,
+      error,
+    });
+  }
+};
