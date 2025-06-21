403de9c4e43abde1e1c0059e59c457059c7b68b2
rakibul-wdp <rakibul.wdp@gmail.com>
2025-06-21 12:31:35 +0600

    create borrow model

---
+++ src/app/models/borrow.model.ts
@@ -0,0 +1,37 @@
+import { model, Schema } from "mongoose";
+import { IBorrow } from "../interfaces/borrow.interface";
+
+const borrowSchema = new Schema<IBorrow>(
+  {
+    book: {
+      type: Schema.Types.ObjectId,
+      ref: "Book",
+      required: [true, "Book reference is required"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Quantity is required"],
+      min: [1, "Quantity must be at least 1"],
+    },
+    dueDate: {
+      type: Date,
+      required: [true, "Due date is required"],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+borrowSchema.post("save", async function (doc) {
+  const Book = model("Book");
+  const book = await Book.findById(doc.book);
+  if (book) {
+    book.copies -= doc.quantity;
+    await book.save();
+    await Book.updateAvailability(doc.book);
+  }
+});
+
+export const Borrow = model<IBorrow>("Borrow", borrowSchema);
