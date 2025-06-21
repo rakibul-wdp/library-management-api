612afe83691018d2566e607b644ad4c67a4a9fa6
rakibul-wdp <rakibul.wdp@gmail.com>
2025-06-21 12:18:05 +0600

    fix file name according to the project requirements

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,17 @@
-import express, { Application, Request, Response } from 'express';
-import { notesRoutes } from './app/controllers/notes.controller';
-import { usersRoutes } from './app/controllers/user.controller';
+import express, { Application, Request, Response } from "express";
+import { notesRoutes } from "./app/controllers/book.controller";
+import { usersRoutes } from "./app/controllers/borrow.controller";
 
 const app: Application = express();
 
-app.use(express.json())
+app.use(express.json());
 
+app.use("/notes", notesRoutes);
+app.use("/users", usersRoutes);
 
-
-app.use("/notes", notesRoutes)
-app.use("/users", usersRoutes)
-
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Note App');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Note App");
 });
-
 
 export default app;
 
