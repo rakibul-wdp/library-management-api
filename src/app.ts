ff471db010fbe8d5718ebb4515ff4ac0da328a4e
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-04 14:32:08 +0600

    add origin

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,11 @@ import cors from "cors";
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 
 app.post("/api/books", createBook);
 app.get("/api/books", getAllBooks);
