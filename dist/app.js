fbd7470ae44a7b3981fb7f052edf7b6a71a91982
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-05 12:20:08 +0600

    cors update with build

--- dist/app.js
+++ dist/app.js
@@ -10,7 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:5173"],
+    origin: ["https://minimal-library-management-system-nu.vercel.app"],
 }));
 app.post("/api/books", book_controller_1.createBook);
 app.get("/api/books", book_controller_1.getAllBooks);
