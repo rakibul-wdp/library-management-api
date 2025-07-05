8eed712c5b7a0d1f1d6d680a731d0f7edb1c20d9
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-05 12:17:59 +0600

    update with live frontend for cors issue

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://minimal-library-management-system-nu.vercel.app"],
   })
 );
 
